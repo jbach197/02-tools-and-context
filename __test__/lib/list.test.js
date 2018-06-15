8d8f80e31a65b48c4b2bb82444e4943c3523b886
Jennifer Bach <jmbach1@gmail.com>
2018-06-15 16:43:35 -0700

    added linter files and cleaned up linter errors

--- __test__/lib/list.test.js
+++ __test__/lib/list.test.js
@@ -2,41 +2,41 @@
 
 const List = require('../../lib/list.js');
 
-describe("List", () => {
+describe('List', () => {
 
-    it("constructor() initializes with length of 0",() => {
-        let list = new List();
-        expect(list.length).toBe(0);
-    });
+  it('constructor() initializes with length of 0',() => {
+    let list = new List();
+    expect(list.length).toBe(0);
+  });
 
-    it("push() increments length by 1",() => {
-        let list = new List();
-        list.push('foo');
-        expect(list.length).toBe(1);
-    });
+  it('push() increments length by 1',() => {
+    let list = new List();
+    list.push('foo');
+    expect(list.length).toBe(1);
+  });
 
-    it("push() inserts items into the list",() => {
-        let list = new List();
-        list.push('foo');
-        list.push('bar');
-        list.push('baz');
-        expect(list[0]).toBe('foo');
-        expect(list[1]).toBe('bar');
-        expect(list[2]).toBe('baz');
-    });
+  it('push() inserts items into the list',() => {
+    let list = new List();
+    list.push('foo');
+    list.push('bar');
+    list.push('baz');
+    expect(list[0]).toBe('foo');
+    expect(list[1]).toBe('bar');
+    expect(list[2]).toBe('baz');
+  });
 
-   // it("pop() decreses length by 1",() => {
-   //     let list = new List();
-   //     list.pop('foo');
-   //     expect(list.length).toBe(-1);
-   // });
+  // it("pop() decreses length by 1",() => {
+  //     let list = new List();
+  //     list.pop('foo');
+  //     expect(list.length).toBe(-1);
+  // });
 
-    it("map() iterates over the list and returns new array",() => {
-        let list = newList();
-        list.push('foo');
-        list.push('bar');
-        list.push('baz');
+  it('map() iterates over the list and returns new array',() => {
+    let list = new List();
+    list.push('foo');
+    list.push('bar');
+    list.push('baz');
 
-        let newList = list.map( (val, i) =>`${i} - ${val}`);
-    });
+    let newList = list.map( (val, i) =>`${i} - ${val}`);
+  });
 });
